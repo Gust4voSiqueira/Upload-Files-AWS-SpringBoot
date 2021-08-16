a921f254d5fc8817598bcecf01bf9304e793f90c
Gustavo Siqueira <gustavosiqueira082@gmail.com>
2021-08-16 00:52:27 -0300

    Primeiro Commit

---
+++ BurgerLets/src/main/java/com/burgerLets/BurgerLetsApplication.java
@@ -0,0 +1,13 @@
+package com.burgerLets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurgerLetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BurgerLetsApplication.class, args);
+	}
+
+}
---
+++ BurgerLets/src/test/java/com/burgerLets/BurgerLetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.burgerLets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurgerLetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
